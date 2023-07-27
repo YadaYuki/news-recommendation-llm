804ddcf5540256ead89b837eb6747c4883174d2f
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-07-27 12:51:07 +0900

    feat: add mind

---
+++ src/mind/MINDDataset.py
@@ -0,0 +1,202 @@
+from torch.utils.data import Dataset
+import torch
+from typing import Tuple, Callable
+import polars as pl
+import numpy as np
+import random
+from utils.logger import logging
+from utils.random_seed import set_random_seed
+
+EMPTY_NEWS_ID, EMPTY_IMPRESSION_IDX = "EMPTY_NEWS_ID", -1
+
+
+class MINDTrainDataset(Dataset):
+    def __init__(
+        self,
+        behavior_df: pl.DataFrame,
+        news_df: pl.DataFrame,
+        batch_transform_texts: Callable[[list[str]], torch.Tensor],
+        npratio: int,
+        history_size: int,
+    ) -> None:
+        self.behavior_df: pl.DataFrame = behavior_df
+        self.news_df: pl.DataFrame = news_df
+        self.batch_transform_texts: Callable[[list[str]], torch.Tensor] = batch_transform_texts
+        self.npratio: int = npratio
+        self.history_size: int = history_size
+
+        self.behavior_df = self.behavior_df.with_columns(
+            [
+                pl.col("impressions")
+                .apply(lambda v: [i for i, imp_item in enumerate(v) if imp_item["clicked"] == 1])
+                .alias("clicked_idxes"),
+                pl.col("impressions")
+                .apply(lambda v: [i for i, imp_item in enumerate(v) if imp_item["clicked"] == 0])
+                .alias("non_clicked_idxes"),
+            ]
+        )
+
+        self.__news_id_to_title_map: dict[str, str] = {
+            self.news_df[i]["news_id"].item(): self.news_df[i]["title"].item() for i in range(len(self.news_df))
+        }
+        self.__news_id_to_title_map[EMPTY_NEWS_ID] = ""
+
+    def __getitem__(
+        self, behavior_idx: int
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:  # TODO: 一行あたりにpositiveが複数存在することも考慮した
+        """
+        Returns:
+            torch.Tensor: history_news
+            torch.Tensor: candidate_news
+            torch.Tensor: labels
+        """
+        # Extract Values
+        behavior_item = self.behavior_df[behavior_idx]
+
+        history: list[str] = (
+            behavior_item["history"].to_list()[0] if behavior_item["history"].to_list()[0] is not None else []
+        )  # TODO: Consider Remove if "history" is None
+        poss_idxes, neg_idxes = (
+            behavior_item["clicked_idxes"].to_list()[0],
+            behavior_item["non_clicked_idxes"].to_list()[0],
+        )
+        EMPTY_IMPRESSION = {"news_id": EMPTY_NEWS_ID, "clicked": 0}
+        impressions = np.array(
+            behavior_item["impressions"].to_list()[0] + [EMPTY_IMPRESSION]
+        )  # NOTE: EMPTY_IMPRESSION_IDX = -1なので最後尾に追加する。
+
+        poss_idxes, neg_idxes = (
+            behavior_item["clicked_idxes"].to_list()[0],
+            behavior_item["non_clicked_idxes"].to_list()[0],
+        )
+
+        # Sampling Positive(clicked) & Negative(non-clicked) Sample
+        sample_poss_idxes, sample_neg_idxes = random.sample(poss_idxes, 1), self.__sampling_negative(
+            neg_idxes, self.npratio
+        )
+
+        sample_impression_idxes = sample_poss_idxes + sample_neg_idxes
+        random.shuffle(sample_impression_idxes)
+
+        sample_impressions = impressions[sample_impression_idxes]
+
+        # Extract candidate_news & history_news based on sample idxes
+        candidate_news_ids = [imp_item["news_id"] for imp_item in sample_impressions]
+        labels = [imp_item["clicked"] for imp_item in sample_impressions]
+        history_news_ids = history[: self.history_size]  # TODO: diverse
+        if len(history) < self.history_size:
+            history_news_ids = history_news_ids + [EMPTY_NEWS_ID] * (self.history_size - len(history))
+
+        # News ID to News Title
+        candidate_news_titles, history_news_titles = [
+            self.__news_id_to_title_map[news_id] for news_id in candidate_news_ids
+        ], [self.__news_id_to_title_map[news_id] for news_id in history_news_ids]
+
+        # Convert to Tensor
+        candidate_news_tensor, history_news_tensor = self.batch_transform_texts(
+            candidate_news_titles
+        ), self.batch_transform_texts(history_news_titles)
+        labels_tensor = torch.Tensor(labels)
+
+        return history_news_tensor, candidate_news_tensor, labels_tensor
+
+    def __len__(self) -> int:
+        return len(self.behavior_df)
+
+    def __sampling_negative(self, neg_idxes: list[int], npratio: int) -> list[int]:
+        if len(neg_idxes) < npratio:
+            return neg_idxes + [EMPTY_IMPRESSION_IDX] * (npratio - len(neg_idxes))
+
+        return random.sample(neg_idxes, self.npratio)
+
+
+class MINDValDataset(Dataset):
+    def __init__(
+        self,
+        behavior_df: pl.DataFrame,
+        news_df: pl.DataFrame,
+        batch_transform_texts: Callable[[list[str]], torch.Tensor],
+        history_size: int,
+    ) -> None:
+        self.behavior_df: pl.DataFrame = behavior_df
+        self.news_df: pl.DataFrame = news_df
+        self.batch_transform_texts: Callable[[list[str]], torch.Tensor] = batch_transform_texts
+        self.history_size: int = history_size
+
+        self.__news_id_to_title_map: dict[str, str] = {
+            self.news_df[i]["news_id"].item(): self.news_df[i]["title"].item() for i in range(len(self.news_df))
+        }
+        self.__news_id_to_title_map[EMPTY_NEWS_ID] = ""
+
+    def __getitem__(
+        self, behavior_idx: int
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:  # TODO: 一行あたりにpositiveが複数存在することも考慮した
+        """
+        Returns:
+            torch.Tensor: history_news
+            torch.Tensor: candidate_news
+            torch.Tensor: labels
+        """
+        # Extract Values
+        behavior_item = self.behavior_df[behavior_idx]
+
+        history: list[str] = (
+            behavior_item["history"].to_list()[0] if behavior_item["history"].to_list()[0] is not None else []
+        )  # TODO: Consider Remove if "history" is None
+        EMPTY_IMPRESSION = {"news_id": EMPTY_NEWS_ID, "clicked": 0}
+        impressions = np.array(
+            behavior_item["impressions"].to_list()[0] + [EMPTY_IMPRESSION]
+        )  # NOTE: EMPTY_IMPRESSION_IDX = -1なので最後尾に追加する。
+
+        # Extract candidate_news & history_news based on sample idxes
+        candidate_news_ids = [imp_item["news_id"] for imp_item in impressions]
+        labels = [imp_item["clicked"] for imp_item in impressions]
+        history_news_ids = history[: self.history_size]  # TODO: diverse
+        if len(history) < self.history_size:
+            history_news_ids = history_news_ids + [EMPTY_NEWS_ID] * (self.history_size - len(history))
+
+        # News ID to News Title
+        candidate_news_titles, history_news_titles = [
+            self.__news_id_to_title_map[news_id] for news_id in candidate_news_ids
+        ], [self.__news_id_to_title_map[news_id] for news_id in history_news_ids]
+
+        # Convert to Tensor
+        candidate_news_tensor, history_news_tensor = self.batch_transform_texts(
+            candidate_news_titles
+        ), self.batch_transform_texts(history_news_titles)
+        labels_tensor = torch.Tensor(labels)
+        logging.info(f"labels_tensor.shape: {labels_tensor.size()}")
+        return history_news_tensor, candidate_news_tensor, labels_tensor
+
+    def __len__(self) -> int:
+        return len(self.behavior_df)
+
+
+if __name__ == "__main__":
+    from utils.mind.dataframe import read_behavior_df, read_news_df
+    from const.path import MIND_SMALL_VAL_DATASET_DIR
+    from transformers import AutoTokenizer
+    from torch.utils.data import DataLoader
+
+    set_random_seed(42)
+
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+    # logging.info()
+    def transform(texts: list[str]) -> torch.Tensor:
+        return tokenizer(texts, return_tensors="pt", max_length=64, padding="max_length", truncation=True)["input_ids"]
+
+    logging.info("Load Data")
+    behavior_df, news_df = read_behavior_df(MIND_SMALL_VAL_DATASET_DIR / "behaviors.tsv"), read_news_df(
+        MIND_SMALL_VAL_DATASET_DIR / "news.tsv"
+    )
+
+    logging.info("Init MINDValDataset")
+    train_dataset = MINDValDataset(behavior_df, news_df, batch_transform_texts=transform, history_size=20)
+    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    logging.info("Start Iteration")
+    for batch in train_dataloader:
+        history_news, candidate_news, labels = batch
+        logging.info(
+            f"history_news.shape: {history_news.shape}, candidate_news.shape: {candidate_news.shape}, labels.shape: {labels.shape}"
+        )
---
+++ src/mind/dataframe.py
@@ -0,0 +1,89 @@
+import polars as pl
+import pandas as pd
+from pathlib import Path
+from typing import Callable
+import inspect
+import json
+import hashlib
+from const.path import CACHE_DIR
+import pickle
+from utils.logger import logging
+
+
+def _cache_dataframe(fn: Callable) -> Callable:
+    def read_df_function_wrapper(*args: tuple, **kwargs: dict) -> pl.DataFrame:
+        # inspect **kwargs
+        bound = inspect.signature(fn).bind(*args, **kwargs)
+        bound.apply_defaults()
+
+        d = bound.arguments
+        d["function_name"] = fn.__name__
+        d["path_to_tsv"] = str(bound.arguments["path_to_tsv"])
+
+        # if file exist in cache path, then load & return it.
+        cache_filename = hashlib.sha256(json.dumps(d).encode()).hexdigest()
+        cache_path = CACHE_DIR / f"{cache_filename}.pth"
+        if cache_path.exists() and (not d["clear_cache"]):
+            with open(cache_path, "rb") as f:
+                df = pickle.load(f)
+            return df
+
+        df = fn(*args, **kwargs)
+
+        cache_path.parent.mkdir(parents=True, exist_ok=True)
+
+        with open(cache_path, "wb") as f:
+            pickle.dump(df, f)
+
+        return df
+
+    return read_df_function_wrapper
+
+
+@_cache_dataframe
+def read_news_df(path_to_tsv: Path, has_entities: bool = False, clear_cache: bool = False) -> pl.DataFrame:
+    # FIXME:
+    # pl.read_csvを直接実行すると、行が欠損するため、pandasでtsvを読み取り、polarsのDataFrameに変換する
+    news_df = pd.read_csv(path_to_tsv, sep="\t", encoding="utf8", header=None)
+    news_df.columns = [
+        "news_id",
+        "category",
+        "subcategory",
+        "title",
+        "abstract",
+        "url",
+        "title_entities",
+        "abstract_entities",
+    ]
+    news_df = pl.from_dataframe(news_df)
+    if has_entities:
+        return news_df
+    return news_df.drop("title_entities", "abstract_entities")
+
+
+@_cache_dataframe
+def read_behavior_df(path_to_tsv: Path, clear_cache: bool = False) -> pl.DataFrame:
+    behavior_df = pl.read_csv(path_to_tsv, separator="\t", encoding="utf8-lossy", has_header=False)
+    behavior_df = behavior_df.rename(
+        {
+            "column_1": "impression_id",
+            "column_2": "user_id",
+            "column_3": "time",
+            "column_4": "history_str",
+            "column_5": "impressions_str",
+        }
+    )
+    behavior_df = (
+        behavior_df.with_columns((pl.col("impressions_str").str.split(" ")).alias("impression_news_list"))
+        .with_columns(
+            [
+                pl.col("impression_news_list")
+                .apply(lambda v: [{"news_id": item.split("-")[0], "clicked": int(item.split("-")[1])} for item in v])
+                .alias("impressions")
+            ]
+        )
+        .with_columns([pl.col("history_str").str.split(" ").alias("history")])
+        .select(["impression_id", "user_id", "time", "history", "impressions"])
+    )
+    logging.info(behavior_df[0])
+    return behavior_df
