e2260b8aa809b9ba5700203cf3a4ce42c560c931
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-07-28 02:22:27 +0900

    fix: fix nrms

--- src/recommendation/nrms/NRMS.py
+++ src/recommendation/nrms/NRMS.py
@@ -3,15 +3,40 @@ import torch
 
 
 class NRMS(nn.Module):
-    def __init__(self, news_encoder: nn.Module, user_encoder: nn.Module) -> None:
+    def __init__(
+        self,
+        news_encoder: nn.Module,
+        user_encoder: nn.Module,
+        hidden_size: int,
+    ) -> None:
         super().__init__()
         self.news_encoder: nn.Module = news_encoder
         self.user_encoder: nn.Module = user_encoder
+        self.hidden_size: int = hidden_size
 
     def forward(self, candidate_news: torch.Tensor, news_histories: torch.Tensor) -> torch.Tensor:
-        news_encoded = self.news_encoder(candidate_news)  # [batch_size, seq_len] -> [batch_size, emb_dim]
+        """
+        Parameters
+        ----------
+        candidate_news : torch.Tensor (shape = (batch_size, 1 + npratio, seq_len))
+        news_histories : torch.Tensor (shape = (batch_size, 1 + npratio, seq_len))
+        Returns
+        ----------
+        output: torch.Tensor (shape = (batch_size, 1 + npratio))
+
+        """
+
+        batch_size, candidate_num, seq_len = candidate_news.size()
+        candidate_news = candidate_news.view(batch_size * candidate_num, seq_len)
+        news_candidate_encoded = self.news_encoder(
+            candidate_news
+        )  # [batch_size * (1 + npratio), seq_len] -> [batch_size * (1 + npratio), emb_dim]
+        news_candidate_encoded = news_candidate_encoded.view(
+            batch_size, candidate_num, self.hidden_size
+        )  # [batch_size * (1 + npratio), emb_dim] -> [batch_size, (1 + npratio), emb_dim]
+
         news_histories_encoded = self.user_encoder(
             news_histories, self.news_encoder
         )  # [batch_size, histories, seq_len] -> [batch_size, emb_dim]
 
-        return torch.sum(news_encoded * news_histories_encoded, dim=1)
+        return torch.bmm(news_candidate_encoded, news_histories_encoded.unsqueeze(-1)).squeeze(-1).squeeze(-1)
--- test/recommendation/nrms/test_nrms.py
+++ test/recommendation/nrms/test_nrms.py
@@ -9,12 +9,19 @@ def test_nrms() -> None:
     pretrained: str = "bert-base-uncased"
     multihead_attn_num_heads: int = 16
     additive_attn_hidden_dim: int = 200
-    batch_size, seq_len, hist_size, emb_dim = 20, 10, 4, AutoConfig.from_pretrained(pretrained).hidden_size
+    batch_size, seq_len, hist_size, candidate_num, emb_dim = (
+        20,
+        10,
+        4,
+        5,
+        AutoConfig.from_pretrained(pretrained).hidden_size,
+    )
+
     plm_news_encoder = PLMBasedNewsEncoder(pretrained, multihead_attn_num_heads, additive_attn_hidden_dim)
     user_encoder = UserEncoder(emb_dim, multihead_attn_num_heads, additive_attn_hidden_dim)
-    plm_based_nrms = NRMS(plm_news_encoder, user_encoder)
+    plm_based_nrms = NRMS(plm_news_encoder, user_encoder, emb_dim)
 
-    candidate_news_batch = torch.arange(batch_size * seq_len).view(batch_size, seq_len)
+    candidate_news_batch = torch.arange(batch_size * candidate_num * seq_len).view(batch_size, candidate_num, seq_len)
     news_histories_batch = torch.arange(batch_size * hist_size * seq_len).view(batch_size, hist_size, seq_len)
 
-    assert tuple(plm_based_nrms(candidate_news_batch, news_histories_batch).size()) == (batch_size,)
+    assert tuple(plm_based_nrms(candidate_news_batch, news_histories_batch).size()) == (batch_size, candidate_num)
