bccb1305c8c77dc9ef3db5b1842d42843374deb5
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-08-08 15:25:23 +0900

    fixed

--- src/config/config.py
+++ src/config/config.py
@@ -4,14 +4,14 @@ from hydra.core.config_store import ConfigStore
 
 @dataclass
 class TrainConfig:
-    pretrained: str = ("distilbert-base-uncased",)
-    npratio: int = (4,)
-    history_size: int = (30,)
-    batch_size: int = (32,)
-    epochs: int = (3,)
-    learning_rate: float = (1e-4,)
-    weight_decay: float = (0.0,)
-    max_len: int = (30,)
+    pretrained: str = "distilbert-base-uncased"
+    npratio: int = 4
+    history_size: int = 30
+    batch_size: int = 32
+    epochs: int = 3
+    learning_rate: float = 1e-4
+    weight_decay: float = 0.0
+    max_len: int = 30
 
 
 cs = ConfigStore.instance()
